050f05bd8bdd6e0ed795b01a9a1989cea3a65075
swade1987 <steven@stevenwade.co.uk>
2017-03-13 12:08:31 +0000

    making sure we add the most recent repo to apt-get

--- terraform/provision.sh
+++ terraform/provision.sh
@@ -2,10 +2,12 @@
 echo "Provisioning boostrap machine ..."
 
 echo "Installing necessary packages ..."
-apt-get update -y
-apt-get upgrade -y
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe"
+sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install -y build-essential git
 sudo apt-get install -qq python2.7 && ln -s /usr/bin/python2.7 /usr/bin/python
+sudo apt-get update -y
 
 echo "Clone the repo"
 git clone https://github.com/swade1987/hands-on-with-kubernetes-workshop.git
