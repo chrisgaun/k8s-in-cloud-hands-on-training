eefd25bee86545efd82f50b1c0a5be9f7dbcf5c8
swade1987 <steven@stevenwade.co.uk>
2017-03-14 19:03:44 +0000

    cloning the correct repo to the bootstrap node

--- terraform/provision.sh
+++ terraform/provision.sh
@@ -10,4 +10,4 @@ sudo apt-get install -qq python2.7 && ln -s /usr/bin/python2.7 /usr/bin/python
 sudo apt-get update -y
 
 echo "Clone the repo"
-git clone https://github.com/swade1987/hands-on-with-kubernetes-workshop.git
+git clone https://github.com/apprenda/hands-on-with-kubernetes-workshop.git
